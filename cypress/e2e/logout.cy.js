e26092ec6a56d49e272c4e7bd292f23a917ba724
Krishna Edwards <krishna.eewards1512@gmail.com>
2022-07-14 10:18:37 -0500

    Logout Issues still happening

--- cypress/e2e/logout.cy.js
+++ cypress/e2e/logout.cy.js
@@ -3,13 +3,15 @@ describe('empty spec', () => {
     cy.visit('https://www.saucedemo.com')
   })
   // This test Suite focuses on logging out
+  // First a user needs to be logged in 
   it('should allow a user to be redirected to the landing page when a correct username and password is entered',()=>{  
     cy.visit('https://www.saucedemo.com'); 
     cy.get('#user-name.input_error.form_input').type('standard_user{enter}');
     cy.get('#password.input_error.form_input').type('secret_sauce{enter}');
     cy.get('#login-button').click();
     cy.url().should('contain', 'https://www.saucedemo.com/inventory.html');
-    // Logout 
+    
+    // Should allow users to Logout successfully 
     //cy.get('.bm-item-list').click()
     // cy.get('.header_label').click()
     // cy.get('#logout.button').click()
